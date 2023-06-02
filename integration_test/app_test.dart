31d0930028af8b3b63a1c7c2d4f10a535f199a3e
Chris Rutkowski <chris@rkw.ski>
2023-06-02 18:05:08 +0400

    updates

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,19 +1,21 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:my_screenshot/main.dart' as app;
+
 import 'local_file_comparator_with_threshold.dart';
 
 void main() {
   final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  final baseDir = (goldenFileComparator as LocalFileComparator).basedir;
-  goldenFileComparator = LocalFileComparatorWithThreshold(
-    Uri.parse('$baseDir/any'), // only baseDir is required
-  )..threshold = 0.001;
-
   group('end-to-end test', () {
     testWidgets('tap on the floating action button, verify counter', (tester) async {
       app.main();
+
+      final baseDir = (goldenFileComparator as LocalFileComparator).basedir;
+      goldenFileComparator = LocalFileComparatorWithThreshold(
+        Uri.parse('$baseDir/any'), // only baseDir is required
+      )..threshold = 0.001;
+
       await tester.pumpAndSettle();
       expect(find.text('0'), findsOneWidget);
       final Finder fab = find.byTooltip('Increment');
